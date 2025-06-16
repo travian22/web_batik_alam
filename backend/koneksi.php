d95c4f2832af43fcffe91f8ca035e4090f00513c
Septaro Travian Gadha <143930628+travian22@users.noreply.github.com>
2025-06-16 11:42:21 +0700

    Update koneksi.php

--- backend/koneksi.php
+++ backend/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'batik_alam_app';
+$host = 'db.be-mons1.bengt.wasmernet.com';
+$username = 'f9c1404b75f080005a8d5db7216f';
+$password = '0684f9c1-404c-742e-8000-c92be5cbf587';
+$dbname = 'dbYj6pKZkYXHZ7TZdq2UE8Aq';
 
 // Koneksi ke MySQL
 $koneksi = new mysqli($host, $username, $password, $dbname);
